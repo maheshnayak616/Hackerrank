476a2fac2b150c93d6eb890f6d778379e618aabe
mahesh.nayak <mahesh.nayak@flipkart.com>
2015-04-26 16:12:35 +0530

    Game of thrones 2

--- Hackerrank/src/GameOfThrones2.py
+++ Hackerrank/src/GameOfThrones2.py
@@ -1,11 +1,26 @@
 import math, sys
 s = sys.stdin.readline().strip()
 se = {}
+m = pow(10,9) + 7
+def factorial(n):
+    fact = 1
+    ans = 1
+    m = pow(10,9) + 7
+    for j in range(1, n+1):
+        fact=(fact*j)%m
+        ans=(ans*fact)%m
+    return ans
+        
 for x in s:
     if x in se: se[x] = se[x] + 1
     else : se[x] = 1
-
-z = math.factorial(len(s) / 2)
+q = len(s) / 2
+print q
+z = factorial(q)
+a = 1
+print z
 for k,v in se.iteritems():
-    z = z / math.factorial(v/2)
-print (z) % (pow(10,9) + 7)
+    a = a *  factorial(v/2)
+print a
+a = a %  m
+print z, a, z / a
